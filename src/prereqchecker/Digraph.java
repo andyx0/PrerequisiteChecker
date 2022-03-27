59a1e286d67633c3ae13be5c7df92ccd9f90cb58
andyx0 <77806295+andyx0@users.noreply.github.com>
2022-03-26 22:48:19 -0400

    Added alternative way to remove semester courses from needToTake

--- src/prereqchecker/Digraph.java
+++ src/prereqchecker/Digraph.java
@@ -100,10 +100,10 @@ public class Digraph {
             for (String course : eligible) {
                 if (needToTake.contains(course)) {
                     sem.add(course);
-                    // needToTake.remove(course);
+                    needToTake.remove(course);
                 }
             }
-            needToTake.removeAll(sem);
+            // needToTake.removeAll(eligible); // alternative way to remove semester courses from needToTake
             completedCourses.addAll(sem);
             plan.add(sem);
         }
